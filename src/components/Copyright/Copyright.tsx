ad791d3bb272f60c4ac7497f0ace87f3a3389740
Clovis Durand <cd.clovel19@gmail.com>
2023-06-14 19:49:48 +0200

    Added Copyright component

---
+++ src/components/Copyright/Copyright.tsx
@@ -0,0 +1,31 @@
+/* Framework imports ----------------------------------- */
+import React from 'react';
+
+/* Component imports ----------------------------------- */
+import Typography from '@mui/material/Typography';
+import MuiLink from '@mui/material/Link';
+
+/* Copyright component --------------------------------- */
+const Copyright: React.FC = () => {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+    >
+      {'Made with ❤️ by '}
+      <MuiLink
+        color="inherit"
+        href="https://github.com/Clovel"
+      >
+        Clovis Durand
+      </MuiLink>
+      {' '}
+      {new Date().getFullYear()}
+      .
+    </Typography>
+  );
+};
+
+/* Export Copyright component -------------------------- */
+export default Copyright;
