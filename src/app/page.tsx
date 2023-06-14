41cce15785b2c0b0180c8f553c7ec51ac37cdfef
Clovis Durand <cd.clovel19@gmail.com>
2023-06-14 23:21:40 +0200

    Updated layout styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,54 +22,36 @@ interface HomePageProps {}
 /* HomePage component ------------------------------- */
 const HomePage: React.FC<HomePageProps> = () => {
   return (
-    <>
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 p-2 lg:dark:bg-zinc-800/30">
-          Liste des événements de la fête de la musique à Bordeaux
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://github.com/Clovel"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By Clovis Durand
-          </a>
-        </div>
-      </div>
+    <div className="flex flex-col place-items-center min-w-full">
+      {
+        Object.entries(
+          reduceEventsByCategory(events)
+        )
+          .map(
+            (categoryEntry, index, array) => {
+              const categoryTitle = categoryEntry[0];
 
-      <div className="relative flex flex-col place-items-center min-w-full">
-        {
-          Object.entries(
-            reduceEventsByCategory(events)
+              return (
+                <>
+                  <section
+                    key={`${categoryTitle}-${index}`}
+                    className="w-full max-w-5xl px-4 g:py-8 mx-auto lg:px-0"
+                  >
+                    <Typography variant="h3">
+                      {categoryTitle}
+                    </Typography>
+                    <EventList events={categoryEntry[1]} />
+                  </section>
+                  {
+                    array.length - 1 !== index &&
+                      <Divider className="w-full" />
+                  }
+                </>
+              );
+            },
           )
-            .map(
-              (categoryEntry, index, array) => {
-                const categoryTitle = categoryEntry[0];
-
-                return (
-                  <>
-                    <section
-                      key={`${categoryTitle}-${index}`}
-                      className="w-full max-w-5xl px-4 py-8 mx-auto lg:px-0"
-                    >
-                      <Typography variant="h3">
-                        {categoryTitle}
-                      </Typography>
-                      <EventList events={categoryEntry[1]} />
-                    </section>
-                    {
-                      array.length - 1 !== index &&
-                        <Divider className="w-full" />
-                    }
-                  </>
-                );
-              },
-            )
-        }
-      </div>
-    </>
+      }
+    </div>
   );
 };
 
